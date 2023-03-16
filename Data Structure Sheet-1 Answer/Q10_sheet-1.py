8aec1bf787f7c00dd902faef3289a9370e7844eb
Shivsaran7776 <116049111+Shivsaran7776@users.noreply.github.com>
2023-03-16 21:52:00 +0530

    Update Q10_sheet-1.py

--- Data Structure Sheet-1 Answer/Q10_sheet-1.py
+++ Data Structure Sheet-1 Answer/Q10_sheet-1.py
@@ -1,7 +1,7 @@
 """Write a C program to read in two numbers, x and n, and then compute the sum of this
 geometric progression: 1+x+x 2 +x 3 +………….+x n
 For example: if n is 3 and x is 5, then the program computes 1+5+25+125. Print x, n, the
-sum."""
+sum.  """
 
 x=int(input("Enter a number for x:"))
 n=int(input("Enter a number for n:"))
